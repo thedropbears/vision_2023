ef40a3b6586c106c8a083862122516a7315a3456
Lucien Morey <lucien@spacemachines.co>
2023-01-13 18:33:01 +1100

    added test_harness

---
+++ test_images.py
@@ -0,0 +1,81 @@
+import csv
+import pytest
+import cv2
+import vision
+from helper_types import BoundingBox, ExpectedGamePiece
+
+
+def read_test_data_csv(fname: str):
+    with open(fname) as f:
+        result = []
+        for image, cone_present, cube_present, x1, y1, x2, y2 in csv.reader(f):
+            result.append(
+                (
+                    image,
+                    bool(cone_present),
+                    bool(cube_present),
+                    int(x1),
+                    int(y1),
+                    int(x2),
+                    int(y2),
+                )
+            )
+    return result
+
+
+images = read_test_data_csv("test/expected.csv")
+
+
+@pytest.mark.parametrize("filename,cone_present,cube_present,x1,x2,y1,y2", images)
+def test_sample_images(
+    filename: str,
+    cone_present: bool,
+    cube_present: bool,
+    x1: int,
+    y1: int,
+    x2: int,
+    y2: int,
+):
+    image = cv2.imread(f"./test/{filename}")
+    assert image is not None
+    bounding_box = BoundingBox(x1, y1, x2, y2)
+    if cone_present:
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CONE)
+            == True
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CUBE)
+            == False
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.BOTH)
+            == True
+        )
+
+    if cube_present:
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CUBE)
+            == True
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CONE)
+            == False
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.BOTH)
+            == True
+        )
+    if not cube_present and not cone_present:
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CUBE)
+            == False
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.CONE)
+            == False
+        )
+        assert (
+            vision.is_game_piece_present(image, bounding_box, ExpectedGamePiece.BOTH)
+            == False
+        )
