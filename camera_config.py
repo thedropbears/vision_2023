9facd1304267ef67933a3df33bf78021243c3770
Jesse Morris <jesse.morris@hullbot.com>
2023-01-16 20:14:04 +1100

    also running pre-commit

--- camera_config.py
+++ camera_config.py
@@ -5,9 +5,9 @@ from wpimath.geometry import Pose3d
 
 @dataclass
 class CameraParams:
-    name: str # the name of the camera 
+    name: str  # the name of the camera
     width: int
     height: int
-    transform: Pose3d # pose of the camera w.r.t to the base link of the robot
-    K: np.ndarray # [3 x 3] intrinsic camera matrix
-    fps: int 
+    transform: Pose3d  # pose of the camera w.r.t to the base link of the robot
+    K: np.ndarray  # [3 x 3] intrinsic camera matrix
+    fps: int
